13efe0e1a0e40bb6bdb2a582aedbcb76073ec30f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-12-20 13:20:05 +0400

    delete endpoint

--- main.py
+++ main.py
@@ -30,17 +30,30 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-#response =requests.post(url=graph_endpoint, json=graph_config, headers = headers)
-#print(response.text)
+response =requests.post(url=graph_endpoint, json=graph_config, headers = headers)
+print(response.text)
 
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-today = datetime(year=2023, month = 12, day=6)
+today = datetime.now()
 print(today.strftime("%Y*%m%d"))
 
 pixel_data = {
     "date" : today.strftime("%Y%m%d"),
-    "quantity": "25",
+    "quantity": input("How many km did you cycle today?"),
 }
 response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+new_pixel_data = {
+    "quantity": "4.5"
+}
+response = requests.put(url = update_endpoint, json = new_pixel_data, headers=headers)
+print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
+
+#https://pixe.la/v1/users/rufo/graphs/graph1.html
 
